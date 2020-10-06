9c08b0888322d004d86ffd5bd14316635ab5612f
corbitt799 <corbitt799@gmail.com>
2020-10-06 17:31:56 -0400

    Update power_button.py

--- power_button/power_button.py
+++ power_button/power_button.py
@@ -8,4 +8,4 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+subprocess.call(['sudo', 'shutdown', '-h', 'now'], shell=False)
